9085a27fcd0b4515ea3ed75a2169a9889f268f04
NievinEvgeny <zhenya-nievin@mail.ru>
2023-05-14 10:22:54 +0700

    Add constexpr qualifiers in RAII wrapper of vector storage

--- include/vector/vector.hpp
+++ include/vector/vector.hpp
@@ -11,36 +11,36 @@ struct VecStorage
     std::size_t capacity = 1;
     T* data;
 
-    VecStorage() : data(static_cast<T*>(operator new(sizeof(T))))
+    constexpr VecStorage() : data(static_cast<T*>(operator new(sizeof(T))))
     {
     }
 
-    ~VecStorage()
+    constexpr ~VecStorage()
     {
         std::destroy_n(data, size);
         operator delete(data);
     }
 
-    void swap(VecStorage<T>& other) noexcept
+    constexpr void swap(VecStorage<T>& other) noexcept
     {
         std::swap(size, other.size);
         std::swap(capacity, other.capacity);
         std::swap(data, other.data);
     }
 
-    VecStorage(const VecStorage<T>& copy)
+    constexpr VecStorage(const VecStorage<T>& copy)
         : size(copy.size), capacity(copy.capacity), data(static_cast<T*>(operator new(sizeof(T) * copy.capacity)))
     {
         std::uninitialized_copy_n(copy.data, copy.size, data);
     }
 
-    VecStorage& operator=(VecStorage<T> other) noexcept
+    constexpr VecStorage& operator=(VecStorage<T> other) noexcept
     {
         other.swap(*this);
         return *this;
     }
 
-    VecStorage(VecStorage<T>&& other) noexcept
+    constexpr VecStorage(VecStorage<T>&& other) noexcept
         : size(std::move(other.size)), capacity(std::move(other.capacity)), data(std::exchange(other.data, nullptr))
     {
     }
