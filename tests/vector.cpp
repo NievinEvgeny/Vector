ce43eeb878fc433a434824658b0e0e52b39fdb64
NievinEvgeny <zhenya-nievin@mail.ru>
2023-05-22 12:03:02 +0700

    Add tests for resize, iterator, erase and insert

--- tests/vector.cpp
+++ tests/vector.cpp
@@ -278,3 +278,386 @@ TEST(Vector, Swap)
         EXPECT_EQ(num2, values1.at(0));
     }
 }
+
+TEST(Vector, DefaultResizeCountBelowSize)
+{
+    constexpr std::size_t new_size = 1;
+    vector::Vector<std::string> vec;
+
+    std::vector<std::string> values{"val1", "val2", "val3"};
+
+    for (auto& val : values)
+    {
+        vec.push_back(std::move(val));
+    }
+
+    vec.resize(new_size);
+
+    EXPECT_THROW(vec.at(2), std::out_of_range);
+}
+
+TEST(Vector, DefaultResizeCountAboveSize)
+{
+    constexpr std::size_t exp_size = 5;
+    constexpr std::size_t new_size = 5;
+    constexpr std::size_t new_index = 4;
+    vector::Vector<std::string> vec;
+
+    std::vector<std::string> values{"val1", "val2", "val3"};
+
+    for (auto& val : values)
+    {
+        vec.push_back(std::move(val));
+    }
+
+    vec.resize(new_size);
+
+    EXPECT_EQ(vec.size(), exp_size);
+    EXPECT_NO_THROW(vec.at(new_index));
+}
+
+TEST(Vector, FillResizeCountAboveSize)
+{
+    constexpr std::size_t exp_size = 10;
+    constexpr std::size_t new_size = 10;
+    constexpr std::size_t new_index = 6;
+    const std::string fill_value = "new value";
+    vector::Vector<std::string> vec;
+
+    std::vector<std::string> values{"val1", "val2", "val3"};
+
+    for (auto& val : values)
+    {
+        vec.push_back(std::move(val));
+    }
+
+    vec.resize(new_size, fill_value);
+
+    EXPECT_EQ(vec.size(), exp_size);
+    EXPECT_EQ(vec.at(new_index), fill_value);
+}
+
+TEST(Iterator, ElementAccess)
+{
+    vector::Vector<std::string> vec;
+
+    std::vector<std::string> values{"val1", "val2", "val3"};
+
+    for (auto& val : values)
+    {
+        vec.push_back(val);
+    }
+
+    vector::Vector<std::string>::iterator iter = vec.begin();
+
+    EXPECT_EQ(*iter, values.at(0));
+    EXPECT_EQ(iter[1], values.at(1));
+    EXPECT_EQ(iter->npos, std::string::npos);
+}
+
+TEST(Iterator, IncrementAndDecrement)
+{
+    vector::Vector<std::string> vec;
+
+    std::vector<std::string> values{"val1", "val2", "val3"};
+
+    for (auto& val : values)
+    {
+        vec.push_back(val);
+    }
+
+    vector::Vector<std::string>::iterator iter = vec.begin();
+    EXPECT_EQ(*(iter++), values.at(0));
+    EXPECT_EQ(*(++iter), values.at(2));
+    EXPECT_EQ(*(iter--), values.at(2));
+    EXPECT_EQ(*(--iter), values.at(0));
+}
+
+TEST(Iterator, ShiftByOffset)
+{
+    constexpr std::size_t offset = 2;
+    vector::Vector<std::string> vec;
+
+    std::vector<std::string> values{"val1", "val2", "val3"};
+
+    for (auto& val : values)
+    {
+        vec.push_back(val);
+    }
+
+    vector::Vector<std::string>::iterator iter = vec.begin();
+    iter += offset;
+    EXPECT_EQ(*iter, values.at(2));
+    iter -= offset;
+    EXPECT_EQ(*iter, values.at(0));
+}
+
+TEST(Iterator, AddAndSubWithOffset)
+{
+    constexpr std::size_t offset = 2;
+    vector::Vector<std::string> vec;
+
+    std::vector<std::string> values{"val1", "val2", "val3"};
+
+    for (auto& val : values)
+    {
+        vec.push_back(val);
+    }
+
+    vector::Vector<std::string>::iterator iter_begin = vec.begin();
+    vector::Vector<std::string>::iterator iter_end = vec.end() - 1;
+    EXPECT_EQ(*(iter_begin + offset), values.at(2));
+    EXPECT_EQ(*(offset + iter_begin), values.at(2));
+    EXPECT_EQ(*(iter_end - offset), values.at(0));
+}
+
+TEST(Iterator, SubIters)
+{
+    vector::Vector<std::string> vec;
+
+    std::vector<std::string> values{"val1", "val2", "val3"};
+
+    for (auto& val : values)
+    {
+        vec.push_back(val);
+    }
+
+    EXPECT_EQ(vec.end() - vec.begin(), vec.size());
+}
+
+TEST(Iterator, Comparison)
+{
+    vector::Vector<std::string> vec;
+
+    std::vector<std::string> values{"val1", "val2", "val3"};
+
+    for (auto& val : values)
+    {
+        vec.push_back(val);
+    }
+
+    vector::Vector<std::string>::iterator iter_begin = vec.begin();
+    vector::Vector<std::string>::iterator iter_end = vec.end();
+
+    EXPECT_TRUE(iter_end > iter_begin);
+    EXPECT_TRUE(iter_end != iter_begin);
+    EXPECT_TRUE(iter_end - vec.size() == iter_begin);
+}
+
+TEST(Vector, EraseSingleElement)
+{
+    vector::Vector<std::string> vec;
+
+    std::vector<std::string> exp_values{"val1", "val3", "val4", "val5", "val6"};
+    std::vector<std::string> values{"val1", "val2", "val3", "val4", "val5", "val6"};
+
+    for (auto& val : values)
+    {
+        vec.push_back(val);
+    }
+
+    vec.erase(vec.begin() + 1);
+
+    for (std::size_t i = 0; i < vec.size(); i++)
+    {
+        EXPECT_EQ(vec.at(i), exp_values.at(i));
+    }
+}
+
+TEST(Vector, EraseRangeOfElements)
+{
+    vector::Vector<std::string> vec;
+
+    std::vector<std::string> exp_values{"val1", "val5", "val6"};
+    std::vector<std::string> values{"val1", "val2", "val3", "val4", "val5", "val6"};
+
+    for (auto& val : values)
+    {
+        vec.push_back(val);
+    }
+
+    vec.erase(vec.begin() + 1, vec.begin() + 4);
+
+    for (std::size_t i = 0; i < vec.size(); i++)
+    {
+        EXPECT_EQ(vec.at(i), exp_values.at(i));
+    }
+}
+
+TEST(Vector, InsertElementCopy)
+{
+    const std::string value = "insert";
+    vector::Vector<std::string> vec;
+
+    std::vector<std::string> exp_values{"val1", "insert", "val2", "val3"};
+    std::vector<std::string> values{"val1", "val2", "val3"};
+
+    for (auto& val : values)
+    {
+        vec.push_back(val);
+    }
+
+    vec.insert(vec.begin() + 1, value);
+
+    for (std::size_t i = 0; i < vec.size(); i++)
+    {
+        EXPECT_EQ(vec.at(i), exp_values.at(i));
+    }
+}
+
+TEST(Vector, InsertElementCopyToEnd)
+{
+    const std::string value = "insert";
+    vector::Vector<std::string> vec;
+
+    std::vector<std::string> exp_values{"val1", "val2", "val3", "insert"};
+    std::vector<std::string> values{"val1", "val2", "val3"};
+
+    for (auto& val : values)
+    {
+        vec.push_back(val);
+    }
+
+    vec.insert(vec.end(), value);
+
+    for (std::size_t i = 0; i < vec.size(); i++)
+    {
+        EXPECT_EQ(vec.at(i), exp_values.at(i));
+    }
+}
+
+TEST(Vector, InsertElementMove)
+{
+    std::string value = "insert";
+    vector::Vector<std::string> vec;
+
+    std::vector<std::string> exp_values{"val1", "insert", "val2", "val3"};
+    std::vector<std::string> values{"val1", "val2", "val3"};
+
+    for (auto& val : values)
+    {
+        vec.push_back(val);
+    }
+
+    vec.insert(vec.begin() + 1, std::move(value));
+
+    for (std::size_t i = 0; i < vec.size(); i++)
+    {
+        EXPECT_EQ(vec.at(i), exp_values.at(i));
+    }
+}
+
+TEST(Vector, InsertElementMoveToEnd)
+{
+    std::string value = "insert";
+    vector::Vector<std::string> vec;
+
+    std::vector<std::string> exp_values{"val1", "val2", "val3", "insert"};
+    std::vector<std::string> values{"val1", "val2", "val3"};
+
+    for (auto& val : values)
+    {
+        vec.push_back(val);
+    }
+
+    vec.insert(vec.end(), std::move(value));
+
+    for (std::size_t i = 0; i < vec.size(); i++)
+    {
+        EXPECT_EQ(vec.at(i), exp_values.at(i));
+    }
+}
+
+TEST(Vector, InsertCountElements)
+{
+    constexpr std::size_t count = 3;
+    std::string value = "insert";
+    vector::Vector<std::string> vec;
+
+    std::vector<std::string> exp_values{"val1", "insert", "insert", "insert", "val2", "val3"};
+    std::vector<std::string> values{"val1", "val2", "val3"};
+
+    for (auto& val : values)
+    {
+        vec.push_back(val);
+    }
+
+    vec.insert(vec.begin() + 1, count, value);
+
+    for (std::size_t i = 0; i < vec.size(); i++)
+    {
+        EXPECT_EQ(vec.at(i), exp_values.at(i));
+    }
+}
+
+TEST(Vector, InsertCountElementsToEnd)
+{
+    constexpr std::size_t count = 3;
+    std::string value = "insert";
+    vector::Vector<std::string> vec;
+
+    std::vector<std::string> exp_values{"val1", "val2", "val3", "insert", "insert", "insert"};
+    std::vector<std::string> values{"val1", "val2", "val3"};
+
+    for (auto& val : values)
+    {
+        vec.push_back(val);
+    }
+
+    vec.insert(vec.end(), count, value);
+
+    for (std::size_t i = 0; i < vec.size(); i++)
+    {
+        EXPECT_EQ(vec.at(i), exp_values.at(i));
+    }
+}
+
+TEST(Vector, InsertRangeElements)
+{
+    constexpr std::size_t range_begin = 3;
+    constexpr std::size_t range_end = 5;
+    vector::Vector<std::string> vec;
+
+    std::vector<std::string> exp_values{"val1", "val4", "val5", "val2", "val3", "val4", "val5", "val6", "val7", "val8"};
+    std::vector<std::string> values{"val1", "val2", "val3", "val4", "val5", "val6", "val7", "val8"};
+
+    for (auto& val : values)
+    {
+        vec.push_back(val);
+    }
+
+    vector::Vector<std::string>::iterator iter_begin = vec.begin() + range_begin;
+    vector::Vector<std::string>::iterator iter_end = vec.begin() + range_end;
+
+    vec.insert(vec.begin() + 1, iter_begin, iter_end);
+
+    for (std::size_t i = 0; i < vec.size(); i++)
+    {
+        EXPECT_EQ(vec.at(i), exp_values.at(i));
+    }
+}
+
+TEST(Vector, InsertRangeElementsToEnd)
+{
+    constexpr std::size_t range_begin = 3;
+    constexpr std::size_t range_end = 5;
+    vector::Vector<std::string> vec;
+
+    std::vector<std::string> exp_values{"val1", "val2", "val3", "val4", "val5", "val6", "val7", "val8", "val4", "val5"};
+    std::vector<std::string> values{"val1", "val2", "val3", "val4", "val5", "val6", "val7", "val8"};
+
+    for (auto& val : values)
+    {
+        vec.push_back(val);
+    }
+
+    vector::Vector<std::string>::iterator iter_begin = vec.begin() + range_begin;
+    vector::Vector<std::string>::iterator iter_end = vec.begin() + range_end;
+
+    vec.insert(vec.end(), iter_begin, iter_end);
+
+    for (std::size_t i = 0; i < vec.size(); i++)
+    {
+        EXPECT_EQ(vec.at(i), exp_values.at(i));
+    }
+}
